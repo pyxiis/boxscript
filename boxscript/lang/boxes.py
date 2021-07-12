f43a897d24990cf9448995343db19bed77172eed
pyxiis <47072520+pyxiis@users.noreply.github.com>
2021-07-12 10:51:06 -0700

    Code style

--- boxscript/lang/boxes.py
+++ boxscript/lang/boxes.py
@@ -30,32 +30,34 @@ ADJACENT = {
     "╣": {"N": "║╣╗", "S": "║╣╝", "W": "═╠"},
 }
 
+
 def neighbors(text, pos):
     r, c = pos
     chars = [[*line] for line in text.splitlines()]
     near = {"N": "\0", "S": "\0", "E": "\0", "W": "\0"}
 
     try:
-        near["E"] = chars[r][c+1]
-    except IndexError:
-        pass
-    
-    try:
-        near["W"] = chars[r][c-1]
+        near["E"] = chars[r][c + 1]
     except IndexError:
         pass
 
     try:
-        near["S"] = chars[r+1][c]
+        near["W"] = chars[r][c - 1]
     except IndexError:
         pass
 
     try:
-        near["N"] = chars[r-1][c]
+        near["S"] = chars[r + 1][c]
+    except IndexError:
+        pass
+
+    try:
+        near["N"] = chars[r - 1][c]
     except IndexError:
         pass
 
     return near
+
 
 def valid(text):
     for i, line in enumerate(text.splitlines()):
@@ -63,9 +65,9 @@ def valid(text):
 
         # check continuity
         for j, char in enumerate(line):
-            if "║" in line[:j] and "║" in line[j+1:]:
+            if "║" in line[:j] and "║" in line[j + 1 :]:
                 continue
-            
+
             expected = ADJACENT.get(char, dict())
 
             neighbor = neighbors(text, (i, j))
