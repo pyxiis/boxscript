9805ff5d7e417be3b5eadb3682be2d1a6d09cc5e
Generic Username <genericusername414+git@gmail.com>
2021-07-16 16:35:38 -0400

    adds tests

--- editor/highlight.py
+++ editor/highlight.py
@@ -5,22 +5,23 @@ from pygments.token import Literal, Token
 class BSLexer(RegexLexer):
     """A Pygments lexer for BoxScript."""
 
-    name = 'BS'
-    aliases = ['box', 'bs', 'boxscript']
-    filenames = ['*.bs']
+    name = "BS"
+    aliases = ["box", "bs", "boxscript"]
+    filenames = ["*.bs"]
 
     tokens = {
-        'root': [
-            (r'[─━│┃┌┍┎┏┐┑┒┓└┗┘┛├┞┟┡┢┣┤┦┧┩┪┫]', Token.Text),
-            (r'[▔░▒▓▚▞◈]', Token.Operator),
-            (r'◇[▄▀]*|[▄▀]+(?=◈)', Literal.String),
-            (r'[▄▀]+', Literal.Number),
-            (r'[▭▯]', Token.Keyword),
-            (r'[▕▏]', Token.Punctuation),
-            (r'║[^\n]*║|[╔╚]═*[╗╝]', Token.Comment),
-            (r'\s', Token.Text.Whitespace),
-            (r'.', Token.Generic)
+        "root": [
+            (r"[─━│┃┌┍┎┏┐┑┒┓└┗┘┛├┞┟┡┢┣┤┦┧┩┪┫]", Token.Text),
+            (r"[▔░▒▓▚▞◈]", Token.Operator),
+            (r"◇[▄▀]*|[▄▀]+(?=◈)", Literal.String),
+            (r"[▄▀]+", Literal.Number),
+            (r"[▭▯]", Token.Keyword),
+            (r"[▕▏]", Token.Punctuation),
+            (r"║[^\n]*║|[╔╚]═*[╗╝]", Token.Comment),
+            (r"\s", Token.Text.Whitespace),
+            (r".", Token.Generic),
         ]
     }
+
 
 # pygments.highlight(t, BSLexer(), Terminal256Formatter(linenos=True))
